089465ba5c77bbacb0b7ca2e7fd2c349e8ac6c8e
hashihana <120104605+hashihana@users.noreply.github.com>
2023-06-19 15:17:39 +0000

    [Update]検索機能

--- app/controllers/admin/spots_controller.rb
+++ app/controllers/admin/spots_controller.rb
@@ -19,10 +19,10 @@ class Admin::SpotsController < ApplicationController
 
     @reviews = Review.all
     @all_rating = '総合評価'
-    @rating1 = '評価1'
-    @rating2 = '評価2'
-    @rating3 = '評価3'
-    @rating4 = '評価4'
+    # @rating1 = '評価1'
+    # @rating2 = '評価2'
+    # @rating3 = '評価3'
+    # @rating4 = '評価4'
    end
   
   def search
--- app/controllers/public/spots_controller.rb
+++ app/controllers/public/spots_controller.rb
@@ -1,14 +1,12 @@
 class Public::SpotsController < ApplicationController
-  
+
   def show
     @spot = Spot.where_genre_active.find(params[:id])
     @review = Review.new
     @reviews = Review.all
     @genres = Genre.only_active
-    # @reviews = Reviews.all
-    
   end
-  
+
   def index
     @genres = Genre.only_active
     if params[:genre_id]
@@ -17,17 +15,28 @@ class Public::SpotsController < ApplicationController
     else
       all_spots = Spot.where_genre_active.includes(:genre)
     end
-    @spots = all_spots.page(params[:page]).per(12)
-    @all_spots_count = all_spots.count
-   end
+      @spots = all_spots.page(params[:page]).per(12)
+      @all_spots_count = all_spots.count
   end
+end
 
   def search
-    @spots = Spot.search(params[:keyword])
+    spots = params[:keyword].present? ? Spot.search(params[:keyword]) : Spot.all
+    spots = spots.where(genre_id: params[:genre_id]) if params[:genre_id].present?
+    spots = spots.where(prefecture_id: params[:prefecture_id]) if params[:prefecture_id].present?
+    @spots = spots.page(params[:page]).per(12)
+
+    @genre = Genre.find_by(id: params[:genre_id])
+    @genres = Genre.only_active
+    @prefectures = Prefecture.all
     @keyword = params[:keyword]
     render "index"
+
+    # @spots = Spot.search(params[:keyword])
+    # @keyword = params[:keyword]
+    # render "index"
   end
-  
+
    private
 
   def spot_params
