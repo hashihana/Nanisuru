89e7c516b6c1879010220fd1ed41413c5b1cbd7e
hashihana <120104605+hashihana@users.noreply.github.com>
2023-06-25 07:06:29 +0000

    [Add]管理者、スポット詳細もっと見る

--- app/javascript/list.js
+++ app/javascript/list.js
@@ -1,9 +1,9 @@
-$(document).on('turbolinks:load', function() {
-  $(document).ready(function () {
-    $('.see-more').on('click', function(){
-      $('.untruncated').removeClass('hide')
-      $('.truncated').addClass("hide")
-      $('see-more').addClass("hide")
-    });
-  });
-});
+// $(document).on('turbolinks:load', function() {
+//   $(document).ready(function () {
+//     $('.see-more').on('click', function(){
+//       $('.untruncated').removeClass('hide')
+//       $('.truncated').addClass("hide")
+//       $('see-more').addClass("hide")
+//     });
+//   });
+// });
