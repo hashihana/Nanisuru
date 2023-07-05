a5f8b0d1b025460ad8c97fd079dc331cbdcd3efe
hashihana <120104605+hashihana@users.noreply.github.com>
2023-07-05 14:27:59 +0000

    [Fix]ユーザー、会員情報編集レイアウト

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,21 +8,21 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_one_attached :profile_image
+  
+  validates :nickname, presence: true
          
    def self.guest
     find_or_create_by!(email: 'guest@example.com') do |customer|
       customer.password = SecureRandom.urlsafe_base64
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
    end
          
    def get_profile_image(width, height)
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  end
-    profile_image.variant(resize_to_limit: [width, height]).processed
-  end
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      profile_image.variant(resize_to_limit: [width, height]).processed
+   end
   
 end
